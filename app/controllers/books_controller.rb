f6e5ebd02b0ddca8869545c5c08f7c402557b2aa
ponxpon <ec2-user@ip-172-31-59-116.ec2.internal>
2021-04-15 11:34:49 +0000

    [Add]bookのshow,editページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,9 +11,11 @@ class BooksController < ApplicationController
   end
 
   def show
+     @book = Book.find(params[:id])
   end
 
   def edit
+     @book = Book.find(params[:id])
   end
 
   def update
